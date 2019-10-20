39041c2abcad251aeb60bfba5803abdb9a32daa1
rayhanuddin2019 <php.devs.ninja@gmail.com>
2019-10-20 22:38:56 +0600

    added import

--- src/re_import.php
+++ src/re_import.php
@@ -14,15 +14,39 @@ Class RE_Import {
       if($item['type'] == 'table'){
       	  $this->table[] = serialize($item);
       }elseif ($item['type'] == 'row') {
-      	$this->table_data[] =serialize($item); 
+      	$this->table_data[] = serialize($item); 
       }else{
-      	$this->params[] =serialize($item); 
-      }       
+      	$this->params[] = serialize($item); 
+      } 
+     
     
     }
 
+    public function get_table_data(){
+    	 return $this->params; 
+    }
+
     public function store(){
-    	
+      global $wpdb;
+     
+        foreach ($this->table_data as $key => $param) {
+            $ns = unserialize( $param );
+            try {
+              if(isset($ns['data'])) {
+                $table  = $ns['data'];
+                 if(isset($table['table'])){
+                    $row = $table['row'];  
+                     $table = $wpdb->prefix.$table['table'];
+                     $wpdb->insert($table,$row); 
+                }
+              } 
+          }
+          catch (exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+          }
+           
+      }
+
     }
 
     
--- src/rui_do_import.php
+++ src/rui_do_import.php
@@ -7,21 +7,28 @@ Class rui_do_import{
      
       
          if( !wp_verify_nonce($_POST['_wpnonce'], 'rui-unyson-import') ) {
-           echo 'Did not save because your form seemed to be invalid. Sorry';
+           
             return;
 
           }
           $this->import();
+         
+          
 
    }
 
    public function import(){
 
-    $parser = new \JsonCollectionParser\Parser();
-	$processor = new \RE_Import();
-	$parser->parse(RUI_DIR.'/database.json', [$processor, 'process']) ;
+         $parser = new \JsonCollectionParser\Parser();
+         $processor = new \RE_Import();
+         $parser->parse(RUI_DIR.'/database.json', [$processor, 'process']);
+         
+         $processor->store();
+        
 
    }
+
+
 }
 
 
